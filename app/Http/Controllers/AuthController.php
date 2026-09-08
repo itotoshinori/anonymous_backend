55929c4b2b353bf8b34d8dfacdb52b70f73f866a
T-I <tito40358@gmail.com>
2026-09-08 17:23:31 +0900

    first-commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            throw ValidationException::withMessages([
+                'email' => ['メールアドレスまたはパスワードが正しくありません。'],
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'ログインしました',
+            'user' => $request->user(),
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json(
+            $request->user()
+        );
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'ログアウトしました',
+        ]);
+    }
+}
